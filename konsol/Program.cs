9a286746f69dd987855234d06f0448ca70964f3c
bbbayazitt <57834689+bbbayazitt@users.noreply.github.com>
2021-08-06 14:08:23 +0300

    Add files via upload

---
+++ konsol/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace konsol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminiz Nedir:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Girin:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
